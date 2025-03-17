58d56190956ea8c831e7be4335a6215cf5a091c8
varchunn <varvaram186@gmail.com>
2025-03-17 18:43:51 +0200

    Initial Commit

---
+++ Transport/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Transport
+//
+//  Created by Варя Мінєнкова on 17.03.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
